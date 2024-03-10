49cd03983df250b9c57646b1eac68058a410ad4b
smesharikk <93827477+smesharikk@users.noreply.github.com>
2024-03-10 16:58:41 +0300

    Согомонян Давит ПИпр

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,76 +6,89 @@
 // Реализация функции для задачи №1
 unsigned int faStr1(const char* str) {
     unsigned int count = 0;
-    bool inWord = false;
+    bool in_word = false;
 
-    // Проходим по строке
     while (*str) {
-        if (std::isalpha(*str)) { // Проверяем, является ли символ буквой
-            if (!inWord) { // Если начинается новое слово
-                inWord = true;
-                count++; // Увеличиваем счетчик слов
+        if (std::isalpha(*str)) { // Проверяем, является ли текущий символ буквой
+            in_word = true; 
+        }
+        else if (std::isspace(*str) or *str == '\0') { // Проверяем, является ли текущий символ пробелом или концом строки
+            if (in_word) { 
+                in_word = false; 
             }
         }
-        else {
-            inWord = false; // Если встречаем не буквенный символ, завершаем слово
+        else if (std::isdigit(*str)) { // Если текущий символ является цифрой
+            in_word = false; // Сбрасываем флаг слова
+            while (*str and !std::isspace(*str)) // Пропускаем все цифры в слове
+                str++;
+            continue;
         }
-        str++;
+        str++; 
     }
 
     return count;
 }
+
 
 // Реализация функции для задачи №2
 unsigned int faStr2(const char* str) {
-    unsigned int count = 0;
-    bool inWord = false;
+    bool in_word = false; // Флаг находимся ли мы внутри слова
+    bool is_uppercase = false; // Флаг является ли текущее слово словом с заглавной буквы
+    int word_count = 0; // Счетчик слов, начинающихся с заглавной буквы и содержащих только строчные буквы
+    int i = 0; 
 
-    // Проходим по строке
-    while (*str) {
-        if (std::isupper(*str)) { // Проверяем, является ли символ заглавной буквой
-            if (!inWord) { // Если начинается новое слово
-                inWord = true;
-                count++; // Увеличиваем счетчик слов
-            }
+    while (str[i] != '\0') {
+        if (str[i] != ' ' and !in_word) { // Если текущий символ не пробел и мы не находимся внутри слова
+            in_word = true;
+            is_uppercase = std::isupper(str[i]); 
         }
-        else if (std::islower(*str) || std::isspace(*str)) {
-            // Если это пробел или строчная латинская буква, продолжаем слово
-            inWord = true;
+        else if (str[i] == ' ' and in_word) { // Если текущий символ пробел и мы находимся внутри слова
+            in_word = false; 
+            is_uppercase = false; 
         }
-        else {
-            inWord = false;
+        else if (!std::islower(str[i]) and in_word and is_uppercase) { // Если текущий символ не строчная буква и это начало слова с заглавной буквы
+            is_uppercase = false; 
+            word_count--; 
         }
-        str++;
+        i++;
     }
 
-    return count;
+    return word_count; 
 }
+
 
 // Реализация функции для задачи №3
 unsigned int faStr3(const char* str) {
-    unsigned int wordCount = 0;
-    unsigned int charCount = 0;
-
-    // Проходим по строке
-    while (*str) {
-        if (std::isalpha(*str)) { // Если символ - буква
-            charCount++; // Увеличиваем счетчик букв в слове
+    float letter_сount = 0; // Счетчик букв
+    int word_сount = 0; // Счетчик слов
+    bool in_word = false; // Флаг, указывающий на наличие слова
+    
+    while (*str != '\0') {
+        if (*str == ' ') {
+            if (in_word) {
+                word_сount++;
+            }
+            in_word = false;
         }
-        else if (std::isspace(*str) && charCount > 0) {
-            wordCount++; // Если встретили пробел и длина слова больше нуля, увеличиваем счетчик слов
-            charCount = 0; // Сбрасываем счетчик букв для нового слова
+        else {
+            in_word = true;
+            letter_сount++;
         }
         str++;
     }
 
-    if (charCount > 0) { // Проверяем, осталось ли слово после конца строки
-        wordCount++;
+    // Учитываем последнее слово, если строка не закончена
+    if (in_word) {
+        word_сount++;
     }
 
-    if (wordCount == 0) {
+    // Если нет слов, средняя длина слова равна 0
+    if (word_сount == 0) {
         return 0;
     }
 
-    // Вычисляем среднюю длину слова и округляем до целого значения по правилам округления
-    return static_cast<unsigned int>(std::round(static_cast<double>(charCount) / wordCount));
+    // Вычисляем среднюю длину слова, округляя до ближайшего целого
+    unsigned int averageLength = round(letter_сount / word_сount);
+
+    return averageLength;
 }
