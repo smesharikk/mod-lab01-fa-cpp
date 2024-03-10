afd07a5ac4c7a13ce77b5d11a2c14fc887293a80
smesharikk <93827477+smesharikk@users.noreply.github.com>
2024-03-10 21:48:46 +0300

    Согомонян Давит ПИпр

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,4 +1,4 @@
-# Copyright 2024 Davit 
+// Copyright 2024 UNN-IASR
 #include "fun.h"
 #include <cctype>
 #include <cmath>
@@ -23,11 +23,11 @@ unsigned int faStr1(const char* str) {
         if (str[i] == ' ' && in_word) {
             in_word = false;
         }
+
         i++;
     }
     return count;
 }
-
 unsigned int faStr2(const char* str) {
     bool in_word = false;
     bool is_good_word = true;
@@ -53,12 +53,10 @@ unsigned int faStr2(const char* str) {
     }
     return count;
 }
-
 unsigned int faStr3(const char* str) {
     float letter_count = 0;
     int word_count = 0;
     bool in_word = false;
-
     while (*str != '\0') {
         if (*str == ' ') {
             if (in_word) {
@@ -72,16 +70,12 @@ unsigned int faStr3(const char* str) {
         }
         str++;
     }
-
     if (in_word) {
         word_count++;
     }
-
     if (word_count == 0) {
         return 0;
     }
-
     unsigned int averageLength = round(letter_count / word_count);
-
     return averageLength;
 }
